825137495d35497122e0d18e8e64a6d5a4d25235
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-10-30 13:46:04 +0100

    Ajouter : les fonctions du GetAllChannels , CreateChannels

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { FastifyAdapter, NestFastifyApplication} from '@nestjs/platform-fastify';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter()
-  ); 
-   await app.listen(process.env.PORT ?? 3000);
+    new FastifyAdapter(),
+  );
+  await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
--- src/modules/channel/channel.service.ts
+++ src/modules/channel/channel.service.ts
@@ -1,36 +1,36 @@
-import { Injectable } from "@nestjs/common";
-import { InjectModel } from "@nestjs/mongoose";
-import { Model } from "mongoose";
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { Channel, ChannelDocument } from './schemas/channel.schema';
-import { CreateChannelDto } from "./dto/createChannel.dto";
-import { UpdateChannelDto } from "./dto/updateChannel.dto";
-
-
+import { CreateChannelDto } from './dto/createChannel.dto';
 
 @Injectable()
 export class ChannelService {
-    
-    constructor(@InjectModel(Channel.name) private channelModel: Model<ChannelDocument>) {}
+  constructor(
+    @InjectModel(Channel.name) private channelModel: Model<ChannelDocument>,
+  ) {}
 
+  async CreateChannel(createChannelDto: CreateChannelDto): Promise<Channel> {
+    const { Title, members, type, badWords } = createChannelDto;
 
-    async create(createChannelDto: CreateChannelDto){
-
+    const newChannel = new this.channelModel({
+      Title,
+      members,
+      type,
+      badWords,
+    });
+    try {
+      return await newChannel.save();
+    } catch (err) {
+      console.error('err', err);
     }
+  }
 
-    async findAll(){
+  async findAll(): Promise<Channel[]> {
+    return await this.channelModel.find().exec();
+  }
 
-    } 
-
-    async findOne(id: string) {
-
-    }
-
-    async update(id: string, updateChannelDto: UpdateChannelDto) {
-
-    }
-
-    async remove(id: string){
-        
-    }
-    
+  async DeleteChanel(id: string): Promise<void> {
+    await this.channelModel.findByIdAndDelete(id).exec();
+  }
 }
