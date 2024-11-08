d5a3a4b002315fbb522370d7b41f54ae8592315f
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-11-08 16:46:06 +0100

    fix : schemas and dto of friend request

--- src/modules/request/friend-request.module.ts
+++ src/modules/request/friend-request.module.ts
@@ -1,11 +1,20 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { FriendRequestService } from './friend-request.service';
-import { FriendRequestController } from './friend-request.controller';
-import { friendRequest, FriendRequestSchema } from './schemas/friend-request.schema';
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { FriendRequestService } from "./friend-request.service";
+import { FriendRequestController } from "./friend-request.controller";
+import {
+  friendRequest,
+  FriendRequestSchema,
+} from "./schemas/friend-request.schema";
+import { User, UserSchema } from "../user/schemas/user.schema";
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: friendRequest.name, schema: FriendRequestSchema }])],
+  imports: [
+    MongooseModule.forFeature([
+      { name: friendRequest.name, schema: FriendRequestSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
+  ],
   controllers: [FriendRequestController],
   providers: [FriendRequestService],
 })
